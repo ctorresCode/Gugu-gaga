9c5adbe01d4a15a0a774f8bed91578a3bcbe5279
Carlos <crtorrescode@gmail.com>
2026-08-23 16:48:44 -0400

    creación del proyecto vozUni

---
+++ foro/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from foro.models import Hilo, Respuesta
+
+# Register your models here.
+admin.site.register(Hilo)
+admin.site.register(Respuesta)
---
+++ foro/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils import timezone
+
+from config import settings
+from usuarios.models import Universidad
+
+# Create your models here.
+class Hilo(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    imagen = models.ImageField(upload_to='hilos/imagenes/', blank=True, null=True) 
+    video = models.FileField(upload_to='hilos/videos/', blank=True, null=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    universidad = models.ForeignKey(Universidad, on_delete=models.CASCADE, null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    ultima_actividad = models.DateTimeField(default=timezone.now) 
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-ultima_actividad']
+
+    def __str__(self):
+        return self.titulo
+
+class Respuesta(models.Model):
+    contenido = models.TextField()
+    imagen = models.ImageField(upload_to='respuestas/imagenes/', blank=True, null=True)
+    video = models.FileField(upload_to='respuestas/videos/', blank=True, null=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    hilo = models.ForeignKey(Hilo, on_delete=models.CASCADE, related_name='respuestas')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)    
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.hilo.ultima_actividad = self.fecha_creacion
+        self.hilo.save()
+
+
+
+
